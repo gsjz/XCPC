a376e9f1464c88a71b3e2f3a64a70225cb438fe3
gsjz-admin <1481550405@qq.com>
2025-06-17 20:51:39 +0800

    upd

--- XCPC_Template_SergioGao/Math/algorithms/number_theory/primitive_root.cpp
+++ XCPC_Template_SergioGao/Math/algorithms/number_theory/primitive_root.cpp
@@ -1,48 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
-int P, g;
-long long modpower(long long x, int idx)
+class FindPrimitiveRoot
 {
-	if(idx==0) return 1;
-	auto res = modpower(x, idx/2);
-	res = res*res%P;
-	if(idx&1) res = res*x%P;
-	return res;
-}
-void findg()
-{
-	if(P==2)
-	{
-		g = 1;
-		return;
-	} 
-	g = -1;
-	vector<int> d;
-	for(int i=1; i*i<= (P-1); i++)
-	{
-		if((P-1)%i!=0) continue;
-		d.push_back(i);
-		if(i!=1 && i*i!=(P-1)) d.push_back( (P-1)/i );
-	}
-	for(int i=2; i<P; i++)
-	{
-		bool flag = 1;
-		for(auto j: d)
-		{
-			if(modpower(i, j)==1) 
-			{
-				flag = 0;
-				break;
-			}
-		}
-		if(flag)
-		{
-			g = i;
-			return;
-		}
-	}
-	return;
-}
+private:
+    int P;
+    long long power_mod_P(long long x, int idx)
+    {
+        long long res = 1;
+        for(long long now = x; idx>0; now = now*now%P, idx>>=1)
+        {
+            if(idx & 1) res = res*now%P;
+        }
+        return res;
+    }
+public:
+    FindPrimitiveRoot(int _P)
+    {
+        P = _P;
+        return;
+    }
+    int work()
+    {
+        if(P==2) return 1;
+        vector<int> d;
+        for(int i=1; i*i<=(P-1); i++)
+        {
+            if( (P-1)%i !=0 ) continue;
+            d.push_back(i);
+            if(i!=1 && i*i!=(P-1) ) d.push_back( (P-1)/i );
+        } // the possible choices to be period must be the divisor of (P-1)
+        for(int i=2; i<P; i++)
+        {
+            bool flag = 1;
+            for(auto j : d)
+            {
+                if(power_mod_P(i, j) == 1)
+                {
+                    flag = 0;
+                    break;
+                } // P-1 isn't the least period
+            }
+            if(flag) return i;
+        } // for prime, the least root is small, so just check from 2
+        return -1; // for legal case, it's not gonna happen
+    }
+};
 int main()
 {
     return 0;
